6afe7ef4711836532daabaaf35e4363128b8dea7
pqw24 <2389636631@qq.com>
2017-03-17 14:53:15 +0800

    Initial commit

---
+++ src/com/xiaowen/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.xiaowen.demo;
+
+/**
+ * Created by lenovo on 2017-03-17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("HelloWorld");
+    }
+}
